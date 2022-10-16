52bc194151ac128ddbdf0faec28fc3d7b90d9eea
kyem-asa <turtlemoons@protonmail.com>
2022-10-15 22:46:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://api.jikan.moe/v4/anime?q=${choice}`
+
+  document.getElementById("result").innerHTML = ""
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON then is a method
+      .then(data => { // what we get from server we pass into parameter of data
+        if (data.data[0].source === "Manga") {
+          document.getElementById("result").innerHTML = `${choice} is based on a manga!`
+        } else {
+          document.getElementById("result").innerHTML = `${choice} is an original anime!`
+        } 
+        
+        // log what we get back from server
+        
+      })
+      .catch(err => { //gives us an error message
+          console.log(`error ${err}`)
+          document.getElementById("result").innerHTML = "Couldn't find anything. Do you want to search again?"
+      });
+
+
+}
+
+
+
+// https://api.jikan.moe/v4/anime?q=Naruto
+
+// data.data[0].source)
